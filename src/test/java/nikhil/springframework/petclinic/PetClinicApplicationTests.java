29e6f028cc5afcf4f61ec481e818b52b6e858c01
Nikhil <nikhil.mollay@gmail.com>
2020-07-14 22:00:24 +0530

    Initial commit

---
+++ src/test/java/nikhil/springframework/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package nikhil.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
