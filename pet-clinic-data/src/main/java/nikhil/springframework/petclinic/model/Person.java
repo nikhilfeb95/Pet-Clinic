a1cdd16a3b80db72e9fdb32b91da8f275857850c
Nikhil <nikhil.mollay@gmail.com>
2020-07-17 15:41:10 +0530

    Added Id properties as long(base entity).closes #10.

--- pet-clinic-data/src/main/java/nikhil/springframework/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/nikhil/springframework/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package nikhil.springframework.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/nikhil/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/nikhil/springframework/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package nikhil.springframework.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private PetType petType;
     private  Owner owner;
     private LocalDate birthDate;
