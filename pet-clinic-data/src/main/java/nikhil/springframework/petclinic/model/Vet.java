4036c0174bdef9b30037778502581668bb0d4d1b
Nikhil <nikhil.mollay@gmail.com>
2020-07-22 19:45:20 +0530

    added Specialty and added to vets. closes #27.

--- pet-clinic-data/src/main/java/nikhil/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/nikhil/springframework/petclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package nikhil.springframework.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    public Set<Specialty> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Specialty> specialities) {
+        this.specialities = specialities;
+    }
+
+    private Set<Specialty> specialities;
 }
