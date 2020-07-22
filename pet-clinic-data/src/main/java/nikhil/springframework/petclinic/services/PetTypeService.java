c169642808578c5b67c1516666ef05fdae0e524d
Nikhil <nikhil.mollay@gmail.com>
2020-07-22 19:55:09 +0530

    Implemented pet type map service. closes #29.

---
+++ pet-clinic-data/src/main/java/nikhil/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package nikhil.springframework.petclinic.services;
+
+import nikhil.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/nikhil/springframework/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package nikhil.springframework.petclinic.services.map;
+
+import nikhil.springframework.petclinic.model.PetType;
+import nikhil.springframework.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findByID(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
