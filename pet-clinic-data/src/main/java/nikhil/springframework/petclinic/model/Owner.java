59566fb70cea805e6272501c78e5ddf6b0b84dbf
Nikhil <nikhil.mollay@gmail.com>
2020-07-22 19:42:22 +0530

    added visits. closes #26.

--- pet-clinic-data/src/main/java/nikhil/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/nikhil/springframework/petclinic/model/Owner.java
@@ -1,4 +1,17 @@
 package nikhil.springframework.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    //Owner has pets
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
