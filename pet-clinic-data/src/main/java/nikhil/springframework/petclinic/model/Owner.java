09080a455ef237f3e1df43be944d32adfa5fee7d
Nikhil <nikhil.mollay@gmail.com>
2020-07-22 19:47:53 +0530

    added contact info to owners. closes #28.

--- pet-clinic-data/src/main/java/nikhil/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/nikhil/springframework/petclinic/model/Owner.java
@@ -7,6 +7,34 @@ public class Owner extends Person {
     //Owner has pets
     private Set<Pet> pets;
 
+    private String address;
+    private String city;
+    private String  telephone;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
     public Set<Pet> getPets() {
         return pets;
     }
