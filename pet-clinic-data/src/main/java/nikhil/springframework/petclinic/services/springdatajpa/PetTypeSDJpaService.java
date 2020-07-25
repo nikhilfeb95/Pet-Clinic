9f7b248bcc96dfca5108c82c750ba78a133a7635
Nikhil <nikhil.mollay@gmail.com>
2020-07-25 23:06:29 +0530

    Created spring data JPA for vets, specialty, pet, petType. closes #40, closes #41, closes #42, closes #43.

---
+++ pet-clinic-data/src/main/java/nikhil/springframework/petclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,48 @@
+package nikhil.springframework.petclinic.services.springdatajpa;
+
+import nikhil.springframework.petclinic.model.PetType;
+import nikhil.springframework.petclinic.repositories.PetTypeRepository;
+import nikhil.springframework.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private  final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findByID(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
---
+++ pet-clinic-data/src/main/java/nikhil/springframework/petclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,47 @@
+package nikhil.springframework.petclinic.services.springdatajpa;
+
+import nikhil.springframework.petclinic.model.Specialty;
+import nikhil.springframework.petclinic.repositories.SpecialtyRepository;
+import nikhil.springframework.petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaService implements SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties :: add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findByID(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
