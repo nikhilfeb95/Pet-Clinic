24fd912b922f4b9f60a4655f79e34c519d95c511
Nikhil <nikhil.mollay@gmail.com>
2020-07-25 18:44:56 +0530

    Converted owner to JPA entity. closes #35.

--- pet-clinic-data/src/main/java/nikhil/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/nikhil/springframework/petclinic/model/Owner.java
@@ -1,15 +1,25 @@
 package nikhil.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
     //Owner has pets --> initializing to prevent null exception
+    //Owner has many pets --> pet has owner(mapped by Owner owner in Pet class)
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String  telephone;
 
     public String getAddress() {
