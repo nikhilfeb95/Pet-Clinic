b5d8892d172d32e0dd4e99a8c6cbf552eb3e1796
Nikhil <nikhil.mollay@gmail.com>
2020-07-25 23:27:42 +0530

    Created visit Map service. closes #46.

---
+++ pet-clinic-data/src/main/java/nikhil/springframework/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package nikhil.springframework.petclinic.services;
+
+import nikhil.springframework.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/nikhil/springframework/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,47 @@
+package nikhil.springframework.petclinic.services.map;
+
+import nikhil.springframework.petclinic.model.Visit;
+import nikhil.springframework.petclinic.services.PetService;
+import nikhil.springframework.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final PetService petService;
+
+    public VisitServiceMap(PetService petService) {
+        this.petService = petService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if(object.getPet() == null || object.getPet().getOwner() == null
+        || object.getPet().getId() == null || object.getPet().getOwner().getId() == null){
+            throw  new RuntimeException("Either owners or pets aren't persisted before hand");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findByID(Long aLong) {
+        return super.findById(aLong);
+    }
+}
