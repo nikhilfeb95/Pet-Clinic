20e8c9115223698b50e431383b252d8901cb10a2
Nikhil <nikhil.mollay@gmail.com>
2020-08-05 22:39:15 +0530

    Implemented a JSON endpoint. closes #71.

--- pet-clinic-web/src/main/java/nikhil/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/nikhil/springframework/petclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package nikhil.springframework.petclinic.controllers;
 
+import nikhil.springframework.petclinic.model.Vet;
 import nikhil.springframework.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -17,5 +21,12 @@ public class VetController {
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    //an api that returns Vets
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson()
+    {
+        return vetService.findAll();
     }
 }
